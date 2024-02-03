50f3b5a308fcf8e750af78f72dac2afbd6ed62fc
Sergio_Montaño <71527413+sergio-jose-aguas-montano@users.noreply.github.com>
2024-02-03 01:16:26 -0600

    [p1.1] - Actualizacion carpetas
    Se actualizó el nombre de las carpetas y archivos trabajados para poder tener un mejor ordenamiento dentro del repositorio.

--- Programas/S02.R
+++
@@ -1,8 +0,0 @@
-
-library(ggplot2)
-
-ggplot(df.Salary.Train) +
- aes(x = YearsExperience, y = Salary) +
- geom_point(shape = "plus", size = 1.5, 
- colour = "#B22222") +
- theme_dark()
---
+++ Rstudio/p1.1_ProcesamientoRegresionLineal/LimpiezaYRegresionLineal.R
@@ -0,0 +1,5 @@
+#Entorno de trabajo
+getwd()
+setwd("E:/Personal/UNI/8vo Semestre/SSPIAII/Trabajos GitHub/SSPIAII.24A.AguasMontanoSergio/Rstudio")
+
+source("S01.Librerias.R")
